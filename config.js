33299a90be49f012152e0de51ec0f51e4db75d73
qhuy2503 <85269773+qhuy2503@users.noreply.github.com>
2021-06-03 11:06:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
-const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+cconst CONFIG = {
+    introTitle: 'vợ cũ à!',
+    introDesc: `anh có một câu hỏi muốn hỏi em`,
+    btnIntro: 'hỏi đi anh',
+    title: 'anh muốn hỏi là',
+    desc: 'em có thương anh không',
+    btnYes: 'có, thương rất nhiều',
+    btnNo: 'không thương',
+    question:'anh thương em nhiều lắm',
+    btnReply: 'Gửi cho anh',
+    reply: 'em rất thương anh',
+    mess: 'anh thương em lắm ớ ><',
+    messDesc: 'thật sự thương nhiều lắm',
+    btnAccept: 'dạ <3',
+    messLink: 'http://fb.com' //https://www.facebook.com/quochuy0106/
 }
